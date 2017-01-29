f269671e0dda9a76be765937975ecfb8da08aaf7
Leonardo Fernandes <loadfms@gmail.com>
2017-01-29 14:20:14 -0200

    Fix

--- actions/product/index.js
+++ actions/product/index.js
@@ -2,7 +2,7 @@ var postgres = require('./../../helpers/postgrecon');
 
 module.exports = {
     get: function (category, callback, app) {
-        var query = 'select p.* from ' + app +'_products p inner join ' + config.app +'_categories_products cp on cp.product_id = p.id inner join ' + config.app +'_categories c on c.id = cp.category_id where p.status = true and c.name = \'' + category + '\'';
+        var query = 'select p.* from ' + app +'_products p inner join ' + app +'_categories_products cp on cp.product_id = p.id inner join ' + app +'_categories c on c.id = cp.category_id where p.status = true and c.name = \'' + category + '\'';
         postgres.doquery(query, callback);
     }
 }
