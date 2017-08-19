2c547e028c65042f3eca173e220ee16fa097d665
Leonardo Fernandes <loadfms@gmail.com>
2017-08-19 16:55:28 -0300

    Inclusao de cover

--- actions/product/index.js
+++ actions/product/index.js
@@ -8,18 +8,22 @@ export class componentProduct {
         var query = 'select * from tb_' + app + '_product order by name'
         postgres.doquery(query, callback);
     }
+    getcover(app, callback) {
+        var query = 'select * from tb_' + app + '_product where cover = true and status = true order by name'
+        postgres.doquery(query, callback);
+    }
     getbycategory(app, category, callback) {
         var query = 'select p.* from tb_' + app + '_product p inner join tb_' + app + '_category_product cp on cp.product_id = p.id inner join tb_' + app + '_category c on c.id = cp.category_id where p.status = true and lower(unaccent(c.name)) = \'' + category + '\' order by p.name'
         postgres.doquery(query, callback);
     }
     save(app, body, callback) {
-        postgres.doquery('insert into tb_' + app + '_product (name, description, image, status) values (\'' + body.name + '\', \'' + body.description + '\', \'' + body.image + '\', true)', () => {
+        postgres.doquery('insert into tb_' + app + '_product (name, description, image, status, cover) values (\'' + body.name + '\', \'' + body.description + '\', \'' + body.image + '\', true, ' + body.cover + ')', () => {
             var query = 'select id from tb_' + app + '_product order by id desc limit 1';
             postgres.doquery(query, callback);
         });
     }
     update(app, body, callback) {
-        postgres.doquery('update tb_' + app + '_product set name = \'' + body.name + '\' , description = \'' + body.description + '\', image = \'' + body.image + '\', status = ' + body.status + ' where id = ' + body.id, callback)
+        postgres.doquery('update tb_' + app + '_product set name = \'' + body.name + '\' , description = \'' + body.description + '\', image = \'' + body.image + '\', status = ' + body.status + ', cover = ' + body.cover + ' where id = ' + body.id, callback)
     }
     delete(app, id, callback) {
         postgres.doquery('update tb_' + app + '_product set status = false where id = ' + id, callback);
@@ -27,6 +31,11 @@ export class componentProduct {
     register(router) {
         router.get('/api/:app/product', (req, res) => {
             this.getall(req.params.app, (items) => {
+                res.json(items);
+            })
+        });
+        router.get('/api/:app/product/cover', (req, res) => {
+            this.getcover(req.params.app, (items) => {
                 res.json(items);
             })
         });
